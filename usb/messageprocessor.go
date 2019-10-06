b7e2cafa3251ca55c2f316af5f5a013c27b336c6
Daniel <daniel.paulus@icloud.com>
2019-10-06 14:06:20 +0200

    improve message handling code

--- usb/messageprocessor.go
+++ usb/messageprocessor.go
@@ -1,6 +1,7 @@
 package usb
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"github.com/danielpaulus/quicktime_video_hack/usb/messages"
 	"github.com/danielpaulus/quicktime_video_hack/usb/packet"
@@ -24,45 +25,50 @@ func NewMessageProcessor(writeToUsb func([]byte), stopSignal chan interface{}) m
 	return mp
 }
 
-var i int
-
 func (mp *messageProcessor) receiveData(data []byte) {
 	log.Debugf("Rcv:\n%s", hex.Dump(data))
 	//TODO: extractFrame(data)
-	if mp.connectionState == initialState {
+	switch binary.LittleEndian.Uint32(data) {
+	case packet.PingPacketMagic:
 		log.Debug("initial ping received, sending ping back")
-		mp.respondToPing(packet.NewPingPacketAsBytes())
-
-		mp.connectionState = pingSent
+		mp.writeToUsb(packet.NewPingPacketAsBytes())
 		return
-	}
-
-	if i == 0 {
-		deviceInfo := packet.NewAsynHpd1Packet(messages.CreateHpd1DeviceInfoDict())
-		log.Debugf("sending: %s", hex.Dump(deviceInfo))
-		mp.writeToUsb(deviceInfo)
-		i++
+	case packet.SyncPacketMagic:
+		mp.handleSyncPacket(data[4:])
 		return
-	}
-	if i == 1 {
-		deviceInfo := packet.NewAsynHpa1Packet(messages.CreateHpd1DeviceInfoDict())
-		log.Debugf("sending: %s", hex.Dump(deviceInfo))
-		mp.writeToUsb(deviceInfo)
-		i++
+	case packet.AsynPacketMagic:
+		mp.handleAsyncPacket(data[4:])
 		return
+	default:
+		log.Warnf("received unknown packet type: %x", data[:4])
 	}
-	if i == 2 {
-		log.Debugf("sending: %s", hex.Dump(packet.AsynNeedPacketBytes))
-		mp.writeToUsb(packet.AsynNeedPacketBytes)
-		return
-	}
-
-	//deviceInfo2 := packet.NewAsynHpa1Packet(messages.CreateHpa1DeviceInfoDict())
 
 	var stop interface{}
 	mp.stopSignal <- stop
 }
 
-func (mp messageProcessor) respondToPing(bytes []byte) {
-	mp.writeToUsb(bytes)
+func (mp *messageProcessor) handleSyncPacket(data []byte) {
+	switch binary.LittleEndian.Uint32(data) {
+	case packet.CWPA:
+		deviceInfo := packet.NewAsynHpd1Packet(messages.CreateHpd1DeviceInfoDict())
+		log.Debugf("sending: %s", hex.Dump(deviceInfo))
+		mp.writeToUsb(deviceInfo)
+
+		deviceInfo1 := packet.NewAsynHpa1Packet(messages.CreateHpd1DeviceInfoDict())
+		log.Debugf("sending: %s", hex.Dump(deviceInfo1))
+		mp.writeToUsb(deviceInfo1)
+	case packet.CVRP:
+	default:
+		log.Warnf("received unknown sync packet type: %x", data[:4])
+	}
+}
+
+func (mp *messageProcessor) handleAsyncPacket(data []byte) {
+	switch binary.LittleEndian.Uint32(data) {
+	case packet.FEED:
+		log.Debugf("sending: %s", hex.Dump(packet.AsynNeedPacketBytes))
+		mp.writeToUsb(packet.AsynNeedPacketBytes)
+	default:
+		log.Warnf("received unknown async packet type: %x", data[:4])
+	}
 }
