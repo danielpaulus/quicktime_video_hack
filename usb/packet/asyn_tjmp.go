10621d5d7ed4592e62411daf503309c3ee5282d1
Daniel <daniel.paulus@icloud.com>
2019-10-11 17:40:19 +0200

    add unknown asyn types

--- usb/packet/asyn_tjmp.go
+++ usb/packet/asyn_tjmp.go
@@ -1,10 +1,29 @@
 package packet
 
-import "github.com/danielpaulus/quicktime_video_hack/usb/dict"
+import (
+	"encoding/binary"
+	"fmt"
+)
 
 type AsynTjmpPacket struct {
 	AsyncMagic  uint32
 	ClockRef    CFTypeID
 	MessageType uint32
-	Property    dict.StringKeyEntry
+	Unknown     []byte
+}
+
+func NewAsynTjmpPacketFromBytes(data []byte) (AsynTjmpPacket, error) {
+	var packet = AsynTjmpPacket{}
+	packet.AsyncMagic = binary.LittleEndian.Uint32(data)
+	if packet.AsyncMagic != AsynPacketMagic {
+		return packet, fmt.Errorf("invalid asyn magic: %x", data)
+	}
+	packet.ClockRef = binary.LittleEndian.Uint64(data[4:])
+	packet.MessageType = binary.LittleEndian.Uint32(data[12:])
+	if packet.MessageType != TJMP {
+		return packet, fmt.Errorf("invalid packet type in asyn tjmp:%x", data)
+	}
+
+	packet.Unknown = data[16:]
+	return packet, nil
 }
