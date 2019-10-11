6cb1ef1ed5dfe23e517af5b4cbdbdf44d10c2e2a
Daniel <daniel.paulus@icloud.com>
2019-10-11 17:39:23 +0200

    improve cmclock

--- usb/coremedia/cmclock.go
+++ usb/coremedia/cmclock.go
@@ -7,12 +7,21 @@ import (
 type CMClock struct {
 	ID        uint64
 	TimeScale uint32
+	startTime time.Time
+}
+
+func NewCMClockWithHostTime(ID uint64) CMClock {
+	return CMClock{
+		ID: ID,
+		//NanoSecond Scale
+		TimeScale: 1000000000,
+		startTime: time.Now(),
+	}
 }
 
 func (c CMClock) GetTime() CMTime {
-	currentTime := time.Now().UnixNano()
 	return CMTime{
-		CMTimeValue: uint64(currentTime),
+		CMTimeValue: uint64(time.Since(c.startTime).Nanoseconds()),
 		CMTimeScale: c.TimeScale,
 		CMTimeFlags: KCMTimeFlags_HasBeenRounded,
 		CMTimeEpoch: 0,
