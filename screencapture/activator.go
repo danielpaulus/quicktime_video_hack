a3d506f7ae9d82f689225c4a8930553e9fe88a59
danielpaulus <daniel.paulus@icloud.com>
2019-11-12 18:53:53 +0100

    Fix multi device support, Fix Mac OS X, Output JSON (#34)
    * add param
    
    * fix docopt
    
    * make devices command output json
    
    * fix log message
    
    * add udid to all methods
    
    * add udid to all methods
    
    * fix activation output
    
    * use correct format specifier for boolean
    
    * fix activate command, fix log output
    
    * refactor usb code
    
    *  fix hanging bug
    
    * remove global context variable
    
    * refactor and clean
    
    * refactor to use err
    
    * improve help text
    
    * remove unused vars

--- screencapture/activator.go
+++ screencapture/activator.go
@@ -2,66 +2,63 @@ package screencapture
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/google/gousb"
 	log "github.com/sirupsen/logrus"
-	"time"
 )
 
 // EnableQTConfig enables the hidden QuickTime Device configuration that will expose two new bulk endpoints.
 // We will send a control transfer to the device via USB which will cause the device to disconnect and then
 // re-connect with a new device configuration. Usually the usbmuxd will automatically enable that new config
 // as it will detect it as the device's preferredConfig.
-func EnableQTConfig(devices []IosDevice) error {
-	for _, device := range devices {
-		err := enableQTConfigSingleDevice(device)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func enableQTConfigSingleDevice(device IosDevice) error {
+func EnableQTConfig(device IosDevice) (IosDevice, error) {
 	udid := device.SerialNumber
-	if isValidIosDeviceWithActiveQTConfig(device.usbDevice.Desc) {
+	ctx := gousb.NewContext()
+	usbDevice, err := ctx.OpenDeviceWithVIDPID(device.VID, device.PID)
+	if err != nil {
+		return IosDevice{}, err
+	}
+	if isValidIosDeviceWithActiveQTConfig(usbDevice.Desc) {
 		log.Debugf("Skipping %s because it already has an active QT config", udid)
-		return nil
+		return device, nil
 	}
 
-	err := sendQTConfigControlRequest(device)
+	err = sendQTConfigControlRequest(usbDevice)
 	if err != nil {
-		return err
+		return IosDevice{}, err
 	}
 
 	var i int
 	for {
-		log.Infof("Checking for active QT config for %s", udid)
-		time.Sleep(500 * time.Millisecond)
+		log.Debugf("Checking for active QT config for %s", udid)
+
 		err = ctx.Close()
 		if err != nil {
 			log.Warn("failed closing context", err)
 		}
+		time.Sleep(500 * time.Millisecond)
 		log.Debug("Reopening Context")
 		ctx = gousb.NewContext()
-		device.usbDevice, err = findBySerialNumber(udid)
+		device, err = device.ReOpen(ctx)
 		if err != nil {
 			log.Debugf("device not found:%s", err)
 			continue
 		}
 		i++
 		if i > 10 {
-			log.Error("Failed activating config")
-			return fmt.Errorf("could not activate Quicktime Config for %s", udid)
+			log.Debug("Failed activating config")
+			return IosDevice{}, fmt.Errorf("could not activate Quicktime Config for %s", udid)
 		}
 		break
 	}
-	log.Infof("QTConfig for %s activated", udid)
-	return err
+	log.Debugf("QTConfig for %s activated", udid)
+	return device, err
 }
 
-func sendQTConfigControlRequest(device IosDevice) error {
+func sendQTConfigControlRequest(device *gousb.Device) error {
 	response := make([]byte, 0)
-	val, err := device.usbDevice.Control(0x40, 0x52, 0x00, 0x02, response)
+	val, err := device.Control(0x40, 0x52, 0x00, 0x02, response)
 
 	if err != nil {
 		log.Warn("Failed sending control transfer for enabling hidden QT config. Seems like this happens sometimes but it still works usually.", err)
@@ -70,9 +67,9 @@ func sendQTConfigControlRequest(device IosDevice) error {
 	return nil
 }
 
-func sendQTDisableConfigControlRequest(device IosDevice) error {
+func sendQTDisableConfigControlRequest(device *gousb.Device) error {
 	response := make([]byte, 0)
-	val, err := device.usbDevice.Control(0x40, 0x52, 0x00, 0x00, response)
+	val, err := device.Control(0x40, 0x52, 0x00, 0x00, response)
 
 	if err != nil {
 		log.Fatal("Failed sending control transfer for disabling hidden QT config", err)
