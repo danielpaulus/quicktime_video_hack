d9ac52aff095e559320847da2169386164b2e41a
Daniel Paulus <daniel.paulus@icloud.com>
2020-02-03 20:47:12 +0100

    fixed broken TTAS

--- screencapture/coremedia/cmsamplebuf_test.go
+++ screencapture/coremedia/cmsamplebuf_test.go
@@ -1,6 +1,7 @@
 package coremedia_test
 
 import (
+	"encoding/binary"
 	"io/ioutil"
 	"log"
 	"testing"
@@ -18,20 +19,24 @@ func TestFeedNoSdat(t *testing.T) {
 
 	if assert.NoError(t, err) {
 		assert.Equal(t, false, sbufPacket.HasFormatDescription)
-		assert.Equal(t, coremedia.KCMTimeFlagsHasBeenRounded, sbufPacket.OutputPresentationTimestamp.CMTimeFlags)
-		assert.Equal(t, uint64(0x44b82fa09), sbufPacket.OutputPresentationTimestamp.Seconds())
-		assert.Equal(t, 1, len(sbufPacket.SampleTimingInfoArray))
-		assert.Equal(t, uint64(0), sbufPacket.SampleTimingInfoArray[0].Duration.Seconds())
-		assert.Equal(t, uint64(0x44b82fa09), sbufPacket.SampleTimingInfoArray[0].PresentationTimeStamp.Seconds())
-		assert.Equal(t, uint64(0), sbufPacket.SampleTimingInfoArray[0].DecodeTimeStamp.Seconds())
-		assert.Equal(t, 56604, len(sbufPacket.SampleData))
-		assert.Equal(t, 1, sbufPacket.NumSamples)
-		assert.Equal(t, 1, len(sbufPacket.SampleSizes))
-		assert.Equal(t, 56604, sbufPacket.SampleSizes[0])
-		assert.Equal(t, 4, len(sbufPacket.Attachments.Entries))
-		assert.Equal(t, 2, len(sbufPacket.Sary.Entries))
+
 	}
 	print(sbufPacket.String())
+}
+
+func TestUnknownMagic(t *testing.T) {
+	dat, err := ioutil.ReadFile("../packet/fixtures/asyn-feed-ttas-only")
+	if err != nil {
+		log.Fatal(err)
+	}
+	binary.LittleEndian.PutUint32(dat[32:], 0x75756c6c)
+
+	_, err = coremedia.NewCMSampleBufferFromBytesVideo(dat[20:])
+
+	if assert.Error(t, err) {
+		assert.Contains(t, err.Error(), "lluu")
+	}
+
 }
 
 func TestCMSampleBuffer(t *testing.T) {
