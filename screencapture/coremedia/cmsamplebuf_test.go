db11fd0317b994dd81df27c4c12894cc96814736
Daniel Paulus <daniel.paulus@contentful.com>
2020-02-03 19:37:16 +0100

    switch computers :-D

--- screencapture/coremedia/cmsamplebuf_test.go
+++ screencapture/coremedia/cmsamplebuf_test.go
@@ -9,6 +9,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestFeedNoSdat(t *testing.T) {
+	dat, err := ioutil.ReadFile("../packet/fixtures/asyn-feed-ttas-only")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sbufPacket, err := coremedia.NewCMSampleBufferFromBytesVideo(dat[20:])
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, false, sbufPacket.HasFormatDescription)
+		assert.Equal(t, coremedia.KCMTimeFlagsHasBeenRounded, sbufPacket.OutputPresentationTimestamp.CMTimeFlags)
+		assert.Equal(t, uint64(0x44b82fa09), sbufPacket.OutputPresentationTimestamp.Seconds())
+		assert.Equal(t, 1, len(sbufPacket.SampleTimingInfoArray))
+		assert.Equal(t, uint64(0), sbufPacket.SampleTimingInfoArray[0].Duration.Seconds())
+		assert.Equal(t, uint64(0x44b82fa09), sbufPacket.SampleTimingInfoArray[0].PresentationTimeStamp.Seconds())
+		assert.Equal(t, uint64(0), sbufPacket.SampleTimingInfoArray[0].DecodeTimeStamp.Seconds())
+		assert.Equal(t, 56604, len(sbufPacket.SampleData))
+		assert.Equal(t, 1, sbufPacket.NumSamples)
+		assert.Equal(t, 1, len(sbufPacket.SampleSizes))
+		assert.Equal(t, 56604, sbufPacket.SampleSizes[0])
+		assert.Equal(t, 4, len(sbufPacket.Attachments.Entries))
+		assert.Equal(t, 2, len(sbufPacket.Sary.Entries))
+	}
+	print(sbufPacket.String())
+}
+
 func TestCMSampleBuffer(t *testing.T) {
 	dat, err := ioutil.ReadFile("../packet/fixtures/asyn-feed")
 	if err != nil {
--- screencapture/packet/asyn_feed_test.go
+++ screencapture/packet/asyn_feed_test.go
@@ -12,19 +12,6 @@ import (
 
 const expectedString = `ASYN_SBUF{ClockRef:7ffb5cc32f60, sBuf:{OutputPresentationTS:CMTime{95911997690984/1000000000, flags:KCMTimeFlagsHasBeenRounded, epoch:0}, NumSamples:1, Nalus:[{len:30 type:SEI},{len:90712 type:IDR},], fdsc:fdsc:{MediaType:Video, VideoDimension:(1126x2436), Codec:AVC-1, PPS:27640033ac5680470133e69e6e04040404, SPS:28ee3cb0, Extensions:IndexKeyDict:[{49 : IndexKeyDict:[{105 : 0x01640033ffe1001127640033ac5680470133e69e6e0404040401000428ee3cb0fdf8f800},]},{52 : H.264},]}, attach:IndexKeyDict:[{28 : IndexKeyDict:[{46 : Float64[2436.000000]},{47 : Float64[2436.000000]},]},{29 : Int32[0]},{26 : IndexKeyDict:[{46 : Float64[1126.000000]},{47 : Float64[2436.000000]},{45 : Float64[0.000000]},{44 : Float64[0.000000]},]},{27 : IndexKeyDict:[{46 : Float64[1126.000000]},{47 : Float64[2436.000000]},{45 : Float64[0.000000]},{44 : Float64[0.000000]},]},], sary:IndexKeyDict:[{4 : %!s(bool=false)},], SampleTimingInfoArray:{Duration:CMTime{1/60, flags:KCMTimeFlagsHasBeenRounded, epoch:0}, PresentationTS:CMTime{95911997690984/1000000000, flags:KCMTimeFlagsHasBeenRounded, epoch:0}, DecodeTS:CMTime{0/0, flags:KCMTimeFlagsValid, epoch:0}}}}`
 
-func TestFeedNoSdat(t *testing.T) {
-	dat, err := ioutil.ReadFile("fixtures/asyn-feed-ttas-only")
-	if err != nil {
-		log.Fatal(err)
-	}
-	feedPacket, err := packet.NewAsynCmSampleBufPacketFromBytes(dat[4:])
-	if assert.NoError(t, err) {
-		assert.Equal(t, uint64(0x7ffb5cc32f60), feedPacket.ClockRef)
-		assert.Equal(t, expectedString, feedPacket.String())
-		assert.Equal(t, coremedia.MediaTypeVideo, feedPacket.CMSampleBuf.MediaType)
-	}
-}
-
 func TestFeed(t *testing.T) {
 	dat, err := ioutil.ReadFile("fixtures/asyn-feed")
 	if err != nil {
