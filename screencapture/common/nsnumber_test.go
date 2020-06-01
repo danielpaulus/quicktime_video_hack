34583bebf727f5acee260489555662bbf6c48d66
ChaCha <jjunghyup@naver.com>
2020-06-01 23:43:20 +0900

    add testcase for "type 5"

--- screencapture/common/nsnumber_test.go
+++ screencapture/common/nsnumber_test.go
@@ -8,12 +8,14 @@ import (
 
 //I took these from hexdumps
 var typeSix = []byte{0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x9E, 0x40}
+var typeFive = []byte{0x05, 0x2E, 00, 00, 00}
 var typeFour = []byte{0x04, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
 var typeThree = []byte{0x03, 0x1E, 00, 00, 00}
 
 const typeSixDecoded float64 = 1920
-const typeThreeDecoded uint32 = 30
+const typeFiveDecoded uint32 = 46
 const typeFourDecoded uint64 = 5
+const typeThreeDecoded uint32 = 30
 
 func TestErrors(t *testing.T) {
 	var broken []byte
@@ -36,6 +38,12 @@ func TestErrors(t *testing.T) {
 	broken[0] = 56
 	_, err = common.NewNSNumber(broken)
 	assert.Error(t, err)
+
+	broken = make([]byte, len(typeFive))
+	copy(broken, typeFive)
+	broken[0] = 134
+	_, err = common.NewNSNumber(broken)
+	assert.Error(t, err)
 }
 
 func TestNumberValue(t *testing.T) {
@@ -45,12 +53,17 @@ func TestNumberValue(t *testing.T) {
 		assert.Equal(t, typeSixDecoded, float64Num.FloatValue)
 	}
 
+	uint32Num, err := common.NewNSNumber(typeFive)
+	if assert.NoError(t, err) {
+		assert.Equal(t, typeFiveDecoded, uint32Num.IntValue)
+	}
+
 	uint64Num, err := common.NewNSNumber(typeFour)
 	if assert.NoError(t, err) {
 		assert.Equal(t, typeFourDecoded, uint64Num.LongValue)
 	}
 
-	uint32Num, err := common.NewNSNumber(typeThree)
+	uint32Num, err = common.NewNSNumber(typeThree)
 	if assert.NoError(t, err) {
 		assert.Equal(t, typeThreeDecoded, uint32Num.IntValue)
 	}
