b453dc9ee13c14d8a6a4f8b8e4d57feb8f2c0a03
Daniel Paulus <daniel.paulus@mesmerhq.com>
2022-08-21 19:55:24 +0200

    add some line breaks

--- windows_activator/main.c
+++ windows_activator/main.c
@@ -18,11 +18,11 @@ int openDev(char *serial);
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        printf("no arguments passed, need device serial");
+        printf("no arguments passed, need device serial\n");
         return 1;
     }
     if (argc > 2) {
-        printf("invalid arguments passed, need device serial");
+        printf("invalid arguments passed, need device serial\n");
         return 1;
     }
     usb_init();
@@ -31,53 +31,52 @@ int main(int argc, char **argv) {
 }
 
 int openDev(char *deviceSerial) {
-    printf("openDev, search: %s", deviceSerial);
+    printf("openDev, search: %s\n", deviceSerial);
     struct usb_bus *bus = NULL;
     struct usb_device *dev = NULL;
     struct usb_dev_handle *udh = NULL;
     int code;
     code = usb_find_busses();
     if (code != 0) {
-        printf("error usb_find_busses: %d", code);
+        printf("error usb_find_busses: %d\n", code);
     }
     code = usb_find_devices();
     if (code != 0) {
-        printf("error usb_find_busses: %d", code);
+        printf("error usb_find_busses: %d\n", code);
     }
     for (bus = usb_get_busses(); bus; bus = bus->next) {
         for (dev = bus->devices; dev; dev = dev->next) {
-            printf("checking device pid: %x vid: %x ", dev->descriptor.idProduct, dev->descriptor.idVendor);
+            printf("checking device pid: %x vid: %x \n", dev->descriptor.idProduct, dev->descriptor.idVendor);
             udh = usb_open(dev);
             if (udh == 0) {
-                printf("error usb_open: pid: %x vid: %x ", dev->descriptor.idProduct, dev->descriptor.idVendor);
+                printf("error usb_open: pid: %x vid: %x \n", dev->descriptor.idProduct, dev->descriptor.idVendor);
                 continue;
             }
             char szSerialNumber[128] = {0};
             code = usb_get_string_simple(udh, dev->descriptor.iSerialNumber, szSerialNumber, sizeof(szSerialNumber));
             if (code != 0) {
-                printf("error usb_get_string_simple: %d", code);
-                continue;
+                printf("error usb_get_string_simple: %d\n", code);
             }
-            printf("cmp: %s with %s", szSerialNumber, deviceSerial);
+            printf("cmp: %s with %s\n", szSerialNumber, deviceSerial);
             if (strcmp(szSerialNumber, deviceSerial) == 0) {
                 #ifdef activate_device
-                printf("match, send control message to activate");
+                printf("match, send control message to activate\n");
                 code = usb_control_msg(udh, 0x40, 0x52, 0x00, 0x02, 0, 0, 1000);
                 #else
                 printf("match, send control message to de-activate");
                 code = usb_control_msg(udh, 0x40, 0x52, 0x00, 0x00, 0, 0, 1000);
                 #endif
                 if (code != 0) {
-                    printf("error usb_control_msg: %d", code);
+                    printf("error usb_control_msg: %d\n", code);
                 }
-                printf("sleep");
+                printf("sleep\n");
                 Sleep(1000);
-                printf("done");
+                printf("done\n");
                 return code;
             }
             code = usb_close(udh);
             if (code != 0) {
-                printf("error usb_close: pid: %x vid: %x ", dev->descriptor.idProduct, dev->descriptor.idVendor);
+                printf("error usb_close: pid: %x vid: %x \n", dev->descriptor.idProduct, dev->descriptor.idVendor);
             }
         }
     }
